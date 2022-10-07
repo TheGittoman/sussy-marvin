b9538b5bac0226ea9519a077fb197f6c60eabea3
Gittoman <anonyme.and.free@protonmail.com>
2022-10-07 23:47:18 +0300

    cleaned up the code delete-messages reordered to be bit cleaner

--- internal/slashCommands/slashCommands.go
+++ internal/slashCommands/slashCommands.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/rand"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
@@ -91,41 +92,47 @@ var (
 		},
 		"delete-messages": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			permission := i.Interaction.Member.Permissions // get interaction user perms
+			content := ""
+			fail := false
 
 			if permission&discordgo.PermissionManageMessages == discordgo.PermissionManageMessages { // if permissions match manage message bit flags
 				options := i.ApplicationCommandData().Options
 
-				if int(options[0].IntValue()) > 100 || int(options[0].IntValue()) < 0 { // checks that the option range is correct, must be some more elegant way of doing this
-					s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-						Type: discordgo.InteractionResponseChannelMessageWithSource,
-						Data: &discordgo.InteractionResponseData{
-							Content: fmt.Sprintf("Invalid input: number must positive and less or equal to 100"),
-						},
-					})
-				} else {
+				if int(options[0].IntValue()) > 100 || int(options[0].IntValue()) < 0 {
+					content += "Invalid input: input < 100 or > 0\n"
+					fail = true
+				}
+
+				if !fail { // if there is no fails continue with the command
 					messages, err := s.ChannelMessages(i.ChannelID, int(options[0].IntValue()), "", "", "")
-					checkError(err)
+					if err != nil {
+						content += err.Error()
+					}
+
 					var messagesToString []string
 					for _, v := range messages {
 						messagesToString = append(messagesToString, v.ID)
 					}
+
 					err = s.ChannelMessagesBulkDelete(i.ChannelID, messagesToString)
-					checkError(err)
-					s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-						Type: discordgo.InteractionResponseChannelMessageWithSource,
-						Data: &discordgo.InteractionResponseData{
-							Content: fmt.Sprintf("messages deleted: %d", int(options[0].IntValue())),
-						},
-					})
+					if err != nil {
+						content += err.Error()
+					}
+
+					if err == nil {
+						content += "messages deleted: " + strconv.Itoa(int(options[0].IntValue()))
+					}
+
 				}
 			} else {
-				s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
-					Type: discordgo.InteractionResponseChannelMessageWithSource,
-					Data: &discordgo.InteractionResponseData{
-						Content: fmt.Sprintf("Not permitted to run this command"),
-					},
-				})
+				content += "you are not permitted to run this command"
 			}
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Content: fmt.Sprintf(content + " this message will be deleted in 5 seconds"),
+				},
+			})
 			removeInteraction(s, i.Interaction)
 		},
 	}
@@ -163,3 +170,28 @@ var (
 // 	var com = readCommands()
 // 	println(com.DefaultMemberPermissions)
 // }
+
+// {
+// 	Name:        "basic-command",
+// 	Description: "description",
+// },
+// {
+// 	Name:        "basic-command-with-files",
+// 	Description: "description",
+// },
+// {
+// 	Name:        "followups",
+// 	Description: "description",
+// },
+// {
+// 	Name:        "localized-command",
+// 	Description: "description",
+// },
+// {
+// 	Name:        "options",
+// 	Description: "description",
+// },
+// {
+// 	Name:        "permission-overview",
+// 	Description: "description",
+// },
